bc7bfabd51c17e8bde94b361ac15cd7c8510d0f3
JC System <juancarlosgallardo87@gmail.com>
2024-08-28 07:16:26 -0400

    Implementando Movimiento del Player Character

--- Scripts/player_character.gd
+++ Scripts/player_character.gd
@@ -1,25 +1,16 @@
-extends CharacterBody2D
+class_name PlayerCharacter extends CharacterBody2D
 
+@export var move_speed: float = 200
+var cardinal_direction: Vector2 = Vector2.DOWN
+var direction: Vector2 = Vector2.ZERO
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+func _ready():
+	pass
 
-
+func _process(delta):
+	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	velocity = direction * move_speed
+	
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
